70c65e3ae742c9f3d07c76f2c1d06d9fc7753b16
kreonara <kreonara@gmail.com>
2026-03-27 10:02:47 -0600

    Agregando Next.js

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+  title: "CashTrackr",
+  description: "Next.js & Express.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="es"
+      className={`${inter.className}`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
